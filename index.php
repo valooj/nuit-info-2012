d340610f03d726ebd7506de703ecc570396465c3
valooj <jean.valeriane@gmail.com>
2012-12-04 09:43:23 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-6">
+        <title>Nom du site éé</title>
+    </head>
+    <body>
+        <?php echo "le site pour la nuit de l'info de l'équipe M'Bot est en cours de construction !";?>
+        <img src="minibot2.png" alt="Photo du logo M'Bot" />
+        <div id="header"><!-- code destiné à l'entête du site -->
+            <?php echo "blabla du header";?>
+        </div>
+        <div id="content"> <!-- code destiné au corps du site  -->
+            <?php echo "blabla du corps de texte";?>
+        </div>
+        <div id="footer"> <!-- code destiné au bas de page -->
+            <?php echo "blabla du footer";?>
+        </div>        
+    </body>
+</html>
