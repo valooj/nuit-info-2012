db4b8e52699465a7ac60cf0ac4a952189397d0b1
valooj <jean.valeriane@gmail.com>
2012-12-05 13:14:59 +0100

    implémentation du css de base
    style.css
    index.php

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ and open the template in the editor.
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" media="all" href="style.css" />
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-6">
         <title>Nom du site éé</title>
     </head>
     <body>
-        <?php echo "le site pour la nuit de l'info de l'équipe M'Bot est en cours de construction !";?>
-        <img src="minibot2.png" alt="Photo du logo M'Bot" />
         <div id="header"><!-- code destiné à l'entête du site -->
+            <?php echo "le site pour la nuit de l'info de l'équipe M'Bot est en cours de construction !";?>
+            <img src="minibot2.png" alt="Photo du logo M'Bot" />
             <?php echo "blabla du header";?>
         </div>
         <div id="content"> <!-- code destiné au corps du site  -->
