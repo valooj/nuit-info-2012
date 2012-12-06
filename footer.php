7215024605ecc5893c24ecb2a217e4cfdd5df606
Lucas D'Agui <lucas.dagui.1991@gmail.com>
2012-12-06 23:13:11 +0100

    footer (liens vides)

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
  <div id="footer"> <!-- code destiné au bas de page -->
             <?php echo "blabla du footer";?>
+     		<li>
+            	<a href="index.php"> Accueil</a>
+                <a href="#"> Mentions légales</a>
+                <a href="#"> Aide</a>
+            </li>
  </div>  
