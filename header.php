dd805437a040bf0ccbf814b2eaacf7301da5a78a
valooj <jean.valeriane@gmail.com>
2012-12-07 05:28:43 +0100

    css5

--- header.php
+++ header.php
@@ -1,13 +1,3 @@
 <div id="header"><!-- code destiné à l'entête du site -->
-    <h1>  </h1>  <?php echo "Tourisme Interactif" ?>
-    <ul> 
-        <li>  
-            <a href="index.php?page=rechercheClient">recherche</a>
-        </li>
-        <li>  
-        <a href="index.php?page=creationArticle">Proposer une activité </a>
-        </li>
     
-    
-    </ul>
 </div>
